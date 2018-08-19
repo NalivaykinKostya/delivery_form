1945967dbf7656d05633955e96d93f4a9c30f773
Konstantin <kacteh@list.ru>
2018-08-19 23:00:17 +0300

    add Foto frame

--- venv/app/process.py
+++ venv/app/process.py
@@ -2,6 +2,15 @@
 
 import requests
 from flask import Flask, render_template, request, jsonify
+import csv
+import collections
+
+def read_dictionary(local_file):
+	with open(local_file, 'r') as f:
+		reader = csv.DictReader(f)
+		posts = [row for row in reader]
+		return posts
+
 
 app = Flask(__name__)
 
@@ -9,25 +18,56 @@ app = Flask(__name__)
 def index():
 	return render_template('form.html')
 
-@app.route('/process', methods=['POST'])
+@app.route('/home')
+def home():
+    headers = ['Наименование структурного подразделения', '', 'Должность', 'ФИО', 'мобильный номер', 
+	'внутренний номер', '№     кабинета', 'e-mail', 'дата рождения']
+    posts = read_dictionary('dataЛист2.csv') # справочник
+    return render_template("home.html",
+        title = 'Home',
+        posts = posts,
+        headers = headers)
+@app.route('/home2')
+def home2():
+    headers = ['ФИО', 'мобильный номер','внутренний номер']
+    posts = read_dictionary('dataЛист2.csv') # справочник
+    adress_list = {item['ФИО'] : {'mtel' : item['мобильный номер'], 'tel' : item['внутренний номер']}  for item in posts}
+    sorted_adress_list = collections.OrderedDict(sorted(adress_list.items(), key=lambda t: t[0]))
+    return render_template("home2.html",
+        title = 'Home',
+        posts = sorted_adress_list,
+        headers = headers)    
+@app.route('/map')
+def map():
+    headers = ['ФИО', 'должность', 'мобильный номер','внутренний номер', '№     кабинета', 'e-mail', 'дата рождения']
+    posts = read_dictionary('dataЛист2.csv') # справочник
+    adress_list = {item['ФИО'] : {'position' : item['Должность'], 'mtel' : item['мобильный номер'], 
+        'tel' : item['внутренний номер'], 'location' : item['№     кабинета'],
+        'mail' : item['e-mail'], 'birthday' : item['дата рождения']}  for item in posts}
+    sorted_adress_list = collections.OrderedDict(sorted(adress_list.items(), key=lambda t: t[0]))
+    return render_template("map.html",
+        title = 'Map',
+        posts = sorted_adress_list,
+        headers = headers)  
+
+@app.route('/process', methods=['GET', 'POST'])
 def process():
-	delivery_type =request.form['delivery_type']
-	company_adress =request.form['company_adress']
-	company =request.form['company']
-	contact_person =request.form['contact_person']
-	contact_phone =request.form['contact_phone']
-	date =request.form['date']   #2018-02-27
-	if company and contact_phone:
-		url='https://docs.google.com/forms/d/e/1FAIpQLSfIoE_PNBI5AMhqkrLQHgwoHvQfBaeTCSJ_kh9suc1UsHZVfg/formResponse?entry.209801231=%s&entry.2105496144=%s+&entry.319827962=%s&entry.627417731=%s&entry.1034927361=%s&entry.1435582898_year=%s&entry.1435582898_month=%s&entry.1435582898_day=%s&fvv=1&draftResponse=%%5Bnull%%2Cnull%%2C%%224042173472159526854%%22%%5D%%0D%%0A&pageHistory=0&fbzx=4042173472159526854'
-		payload = (delivery_type, company_adress, company,contact_person, contact_phone,date.split('-')[0],date.split('-')[1], date.split('-')[2])
-		r = requests.post(url%(payload))
-		print(r)
+    name = request.form['name']
+    name = name.capitalize()
+    headers = ['ФИО', 'должность', 'мобильный номер','внутренний номер', '№     кабинета', 'e-mail', 'дата рождения']
+    posts = read_dictionary('dataЛист2.csv') # справочник
+    adress_list = {item['ФИО'] : {'position' : item['Должность'], 'mtel' : item['мобильный номер'], 
+        'tel' : item['внутренний номер'], 'location' : item['№     кабинета'],
+        'mail' : item['e-mail'], 'birthday' : item['дата рождения']}  for item in posts}
+    f_dict = {item : adress_list.get(item) for item in adress_list.keys() if name in item}   
+    sorted_adress_list = collections.OrderedDict(sorted(f_dict.items(), key=lambda t: t[0]))
 
-		return jsonify({'name' : 'Success!'})
-
-	return jsonify({'error' : 'Missing data!'})
+    return render_template("map.html",
+        title = 'Map',
+        posts = sorted_adress_list,
+        headers = headers)  
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', debug=True)
 
 
